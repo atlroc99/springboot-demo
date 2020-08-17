d27e87c5898f10f2067807fb071655e47e0b6ac9
mzaman00 <mzaman@revegy.com>
2020-08-16 22:02:02 -0400

    added dev_test branch and put back commented out code

--- src/main/java/com/apress/demo/DemoApplication.java
+++ src/main/java/com/apress/demo/DemoApplication.java
@@ -12,6 +12,40 @@ import java.util.Arrays;
 public class DemoApplication {
 
     public static void main(String [] args) {
+                                   /* SpringApplication app = new SpringApplication(DemoApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);*/
+
+        //make sure to pass the application main class in the constructor
+//       SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoApplication.class);
+
+        //customizing the banner:
+   /*    builder.bannerMode(Banner.Mode.LOG)
+               .sources(DemoApplication.class)
+               .run(args);*/
+
+        //customizing the logger info
+        // builder.logStartupInfo(true).run(args);
+
+
+        //adding application event listener
+     /*   Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+        builder.listeners(new ApplicationListener<ApplicationEvent>() {
+            @Override
+            public void onApplicationEvent(ApplicationEvent event) {
+                logger.info("### --> " + event.getClass().getCanonicalName());
+            }
+        }).run(args);*/
+
+        //Spring-boot guesses what kind of application we are running based on the classpath
+//        builder.web(WebApplicationType.NONE).run(args);
+
+        // passing args to the spring application
+        //if you run your spring application you have to pass arguments to --<args> and --enabled during the start up
+        //-Dspring-boot.run.arguments="args1 args2 arg..n"
+
+//        builder.run(args);
+
 
         /** Setting active profiles
         if no profile is specified then default profile will used
